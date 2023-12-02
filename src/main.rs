6687ba856f76f598b5c46375d6ea41f1ac87fed4
Lucas Fidalgo <lukefidalgo@outlook.com>
2023-12-01 21:10:41 -0300

    part 2

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,51 @@ where P: AsRef<Path>, {
 
 fn parse_digits(s: &str) -> u32 {
     let mut digits: Vec<u32> = Vec::new();
-    for ch in s.chars() {
+
+    let mut str = s.to_string().clone();
+    let len = str.len();
+    let mut i = 0;
+    
+    while i < len {
+        let ch = str.chars().nth(0).unwrap();
+        i += 1;
         if ch.is_digit(10) {
             if let Some(digit) = ch.to_digit(10) {
                 digits.push(digit);
             }
         }
+
+        match ch {
+            'o' => if str.starts_with("one") {
+                digits.push(1);
+            },
+            't' => if str.starts_with("two") {
+                digits.push(2);
+            } else if str.starts_with("three") {
+                digits.push(3);
+            },
+            'f' => if str.starts_with("four") {
+                digits.push(4);
+            } else if str.starts_with("five") {
+                digits.push(5);
+            },
+            's' => if str.starts_with("six") {
+                digits.push(6);
+            } else if str.starts_with("seven") {
+                digits.push(7);
+            },
+            'e' => if str.starts_with("eight") {
+                digits.push(8);
+            },
+            'n' => if str.starts_with("nine") {
+                digits.push(9);
+            },
+            _ => {
+
+            }
+        }
+
+        str.remove(0);
     }
     let first = digits[0];
     let last = digits.last().copied();
